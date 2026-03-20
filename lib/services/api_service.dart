4837d18efff5a985c6f929b6a89f610796bd0fb4
DanielDiMarco007 <dimarcodaniel0306@gmail.com>
2026-03-20 13:11:39 -0500

    crear filtro de estado de personajes

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,6 +5,7 @@ import '../models/character.dart';
 class ApiService {
   final String url = "https://rickandmortyapi.com/api/character";
 
+  //  Obtener personajes
   Future<List<Character>> fetchCharacters() async {
     final response = await http.get(Uri.parse(url));
 
@@ -12,10 +13,14 @@ class ApiService {
       final data = json.decode(response.body);
       final List results = data['results'];
 
-
       return results.map((e) => Character.fromJson(e)).toList();
     } else {
       throw Exception('Error al cargar personajes');
     }
+  }
+
+  //  Filtrar por estado (Alive, Dead, unknown)
+  List<Character> filterByStatus(List<Character> list, String status) {
+    return list.where((c) => c.status == status).toList();
   }
 }
